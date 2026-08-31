b3dfb1e1ea88666f3046bd3567abdbe8eb55f5e3
uvags <uvadev@yandex.ru>
2026-08-31 19:31:24 +0700

    Added main functionality

---
+++ src/ru/yandex/practicum/InvalidWordLengthException.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum;
+
+public class InvalidWordLengthException extends Exception {
+
+    public InvalidWordLengthException(String message) {
+        super(message);
+    }
+}
